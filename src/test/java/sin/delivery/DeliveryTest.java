cf7a4d34e2adfa9f84e135accc03e6422bad1c4a
ravinath <ravibar1@jaganathan.co.uk>
2019-08-31 22:36:31 +0100

    Initial Test Code

---
+++ src/test/java/sin/delivery/DeliveryTest.java
@@ -0,0 +1,10 @@
+package sin.delivery;
+
+import org.junit.Test;
+
+public class DeliveryTest {
+    @Test
+    public void allTest() {
+        throw new UnsupportedOperationException("Code is not developed yet");
+    }
+}
