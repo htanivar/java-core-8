1d70cf9666295454f9533c542c0a981d7f2b201c
Ravi J <raviregi@gmail.com>
2021-10-16 21:30:30 +0530

    code refactored

--- src/test/java/sin/java/expectederror/ExpectedErrorTest.java
+++ src/test/java/sin/java/expectederror/ExpectedErrorTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import sin.java.read.propertyfile.ReadPropertyFile;
 
 
+import java.io.IOException;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
@@ -12,12 +14,12 @@ public class ExpectedErrorTest {
     ReadPropertyFile readPropertyFile = new ReadPropertyFile("myproperties.properties");
 
     @Test(expected = IllegalArgumentException.class)
-    public void expectIllegalArgumentException_Test() {
+    public void expectIllegalArgumentException_Test() throws IOException {
         assertThat(readPropertyFile.readMyProperty("my-invalid-url"), is(""));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void illegalArgumentException_Test() {
+    public void illegalArgumentException_Test() throws IOException {
         ReadPropertyFile illegalArgumentException = new ReadPropertyFile("idontexist");
         assertThat(illegalArgumentException.readMyProperty("my-invalid-url"), is(""));
     }
