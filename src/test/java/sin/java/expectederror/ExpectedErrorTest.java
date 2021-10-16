99c70431abc72a98e32e2a5c18b21fe2e69574ec
htanivar <37423501+htanivar@users.noreply.github.com>
2021-10-16 08:39:04 +0530

    code refactored (#11)
    code refactored
    
    code refactored

---
+++ src/test/java/sin/java/expectederror/ExpectedErrorTest.java
@@ -0,0 +1,18 @@
+package sin.java.expectederror;
+
+import org.junit.Test;
+import sin.java.readpropertyfile.propertyfile.ReadPropertyFile;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ExpectedErrorTest {
+
+    ReadPropertyFile readPropertyFile = new ReadPropertyFile();
+
+    @Test (expected = IllegalArgumentException.class)
+    public void expectIllegalArgumentException_Test() {
+        assertThat(readPropertyFile.readMyProperty("my-invalid-url"), is(""));
+    }
+
+}
--- src/test/java/sin/java/interfaces/MovementImplOldStyleTest.java
+++ src/test/java/sin/java/interfaces/MovementImplOldStyleTest.java
@@ -12,10 +12,4 @@ public class MovementImplOldStyleTest {
         MovementImplOldStyle movementImplOldStyle = new MovementImplOldStyle();
         assertThat(movementImplOldStyle.howOldStyle()).isEqualTo("I am moving from Implementation Class - Old Style");
     }
-
-    @Test
-    public void test_howDefaultNewStyle() {
-        MovementImplOldStyle movementImplOldStyle = new MovementImplOldStyle();
-        assertThat(movementImplOldStyle.how()).isEqualTo("I am implemented in Interface with default - New Style");
-    }
 }
